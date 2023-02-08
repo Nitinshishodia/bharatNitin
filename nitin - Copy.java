0eec5ca64a0147988aa76d29f287fa985117d41b
Nitin shishodia <nitinshishodia123@gmail,com>
2023-02-08 21:32:30 +0530

    file commit

---
+++ nitin - Copy.java
@@ -0,0 +1,11 @@
+class demo
+{
+public static void main(String args[])
+{
+System.out.println("hallo world!");
+}
+public static void main(String args[])
+{
+System.out.println("hallo world!");
+}
+}
